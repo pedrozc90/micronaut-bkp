a57b54300a4f8174939556a21a0fc65a465f19f1
pedrozc90 <pedrozc90@gmail.com>
2022-09-16 15:27:25 -0300

    update swagger-ui

--- src/main/java/com/pedrozc90/Application.java
+++ src/main/java/com/pedrozc90/Application.java
@@ -1,25 +1,25 @@
 package com.pedrozc90;
 
-//import io.micronaut.openapi.annotation.OpenAPIInclude;
 import io.micronaut.runtime.Micronaut;
-//import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-//import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
-//import io.swagger.v3.oas.annotations.info.Info;
-//import io.swagger.v3.oas.annotations.security.SecurityScheme;
-//import io.swagger.v3.oas.annotations.tags.Tag;
-//
-//@SecurityScheme(
-//    name = "BearerAuth",
-//    type = SecuritySchemeType.HTTP,
-//    scheme = "bearer",
-//    bearerFormat = "jwt"
-//)
-//@OpenAPIDefinition(
-//    info = @Info(
-//        title = "blank",
-//        version = "1.0"
-//    )
-//)
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+
+@SecurityScheme(
+    name = "BearerAuth",
+    type = SecuritySchemeType.HTTP,
+    scheme = "bearer",
+    bearerFormat = "jwt",
+    paramName = "token"
+)
+@OpenAPIDefinition(
+    info = @Info(
+        title = "blank",
+        version = "1.0"
+    )
+)
 //@OpenAPIInclude(
 //    classes = {
 //        io.micronaut.security.endpoints.LoginController.class,
@@ -28,6 +28,7 @@ import io.micronaut.runtime.Micronaut;
 //    },
 //    tags = @Tag(name = "Security")
 //)
+@SecurityRequirement(name = "BearerAuth")
 public class Application {
 
     public static void main(final String[] args) {
